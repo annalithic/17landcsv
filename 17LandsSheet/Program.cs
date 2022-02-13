f4907904e6e94028193cc1e80b0ba7dd4041c08a
FloorBelow <floorbelow@zoho.com>
2022-02-13 19:27:29 +1300

    initial

---
+++ 17LandsSheet/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _17LandsSheet {
+    class Program {
+
+        struct Card : IComparable<Card> {
+            public string name;
+            public string color;
+            public string rarity;
+
+            public int seen;
+            public float alsa;
+
+            public int taken;
+            public float ata;
+
+            public int gp;
+            public float gpwr;
+
+            public int oh;
+            public float ohwr;
+
+            public int gd;
+            public float gdwr;
+
+            public int gih;
+            public float gihwr;
+
+            public int gnd;
+            public float gndwr;
+
+            public float iwd;
+
+            public int CompareTo(Card other) {
+                if (gihwr > other.gihwr) return -1;
+                if (gihwr == other.gihwr) return 0;
+                return 1;
+            }
+
+            public override string ToString() {
+                return $"{name} {color} {rarity} {seen} {alsa} {taken} {ata} {gp} {gpwr}";
+            }
+
+            
+        }
+
+        static void Main(string[] args) {
+
+            
+
+            List<Card> cards = new List<Card>();
+            string[] lines = File.ReadAllLines("data.txt");
+            for (int i = 0; i < lines.Length; i += 2) {
+
+                Card c = new Card();
+                c.name = lines[i];
+                string[] words = lines[i + 1].Split('\t');
+                c.color = words[1];
+                c.rarity = words[2];
+
+                c.seen = ParseInt(words[3]);
+                c.alsa = ParseFloat(words[4]);
+
+                c.taken = ParseInt(words[5]);
+                c.ata = ParseFloat(words[6]);
+
+                c.gp = ParseInt(words[7]);
+                c.gpwr = ParsePercent(words[8]);
+
+                c.oh = ParseInt(words[9]);
+                c.ohwr = ParsePercent(words[10]);
+
+                c.gd = ParseInt(words[11]);
+                c.gdwr = ParsePercent(words[12]);
+
+                c.gih = ParseInt(words[13]);
+                c.gihwr = ParsePercent(words[14]);
+
+                c.gnd = ParseInt(words[15]);
+                c.gndwr = ParsePercent(words[16]);
+
+                c.iwd = ParsePercent(words[17], 'p');
+
+                cards.Add(c);
+            }
+
+            //foreach (Card card in cards) Console.WriteLine($"{card.name}|{card.ata}|{card.gihwr}|{card.iwd}");
+            CreateSheet(cards);
+        }
+
+        static void CreateSheet(List<Card> cards) {
+            List<Card> w = new List<Card>();
+            List<Card> u = new List<Card>();
+            List<Card> b = new List<Card>();
+            List<Card> r = new List<Card>();
+            List<Card> g = new List<Card>();
+            List<Card> m = new List<Card>();
+
+            cards.Sort();
+
+            foreach (Card card in cards) {
+                if (card.rarity != "C" && card.rarity != "U") continue;
+                switch(card.color) {
+                    case "W": w.Add(card); break;
+                    case "U": u.Add(card); break;
+                    case "B": b.Add(card); break;
+                    case "R": r.Add(card); break;
+                    case "G": g.Add(card); break;
+                    default: m.Add(card); break;
+                }
+            }
+
+            Console.WriteLine("WHITE|ATA|WR|IWD||BLUE|ATA|WR|IWD||BLACK|ATA|WR|IWD||RED|ATA|WR|IWD||GREEN|ATA|WR|IWD|||MULTI|ATA|WR|IWD||W|U|B|R|G|M");
+
+            int line = 0;
+            bool keepDoingLines = true;
+            while(keepDoingLines) {
+                keepDoingLines = false;
+                if (line < w.Count) { keepDoingLines = true; WriteCard(w[line]); } else Console.Write("|||||");
+                if (line < u.Count) { keepDoingLines = true; WriteCard(u[line]); } else Console.Write("|||||");
+                if (line < b.Count) { keepDoingLines = true; WriteCard(b[line]); } else Console.Write("|||||");
+                if (line < r.Count) { keepDoingLines = true; WriteCard(r[line]); } else Console.Write("|||||");
+                if (line < g.Count) { keepDoingLines = true; WriteCard(g[line]); } else Console.Write("|||||");
+                if (line < m.Count) { keepDoingLines = true; Console.Write(m[line].color + "|"); WriteCard(m[line]); } else Console.Write("|||||");
+                Console.Write("|");
+                if (line < w.Count) Console.Write(w[line].rarity + "|"); else Console.Write("|");
+                if (line < u.Count) Console.Write(u[line].rarity + "|"); else Console.Write("|");
+                if (line < b.Count) Console.Write(b[line].rarity + "|"); else Console.Write("|");
+                if (line < r.Count) Console.Write(r[line].rarity + "|"); else Console.Write("|");
+                if (line < g.Count) Console.Write(g[line].rarity + "|"); else Console.Write("|");
+                if (line < m.Count) Console.Write(m[line].rarity + "|"); else Console.Write("|");
+                line++;
+                Console.WriteLine();
+            }
+        }
+
+        static void WriteCard(Card card) {
+            Console.Write($"{card.name}|{card.ata}|{card.gihwr}|{card.iwd}||");
+        }
+
+        static int ParseInt(string str) {
+            if (str == "") return 0;
+            return int.Parse(str);
+        }
+
+        static float ParseFloat(string str) {
+            if (str == "") return 0;
+            return float.Parse(str);
+        }
+
+
+        static float ParsePercent(string str, char trail = '%') {
+            if (str == "") return 0;
+            return float.Parse(str.TrimEnd(trail)) / 100;
+        }
+    }
+
+
+}
